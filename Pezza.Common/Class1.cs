fb32260290d9744d4d994c782dd030049f5c4f27
Ryan Naidoo <ryan.naidoo@entelect.co.za>
2022-07-14 13:13:09 +0200

    Add project files.

---
+++ Pezza.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pezza.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Pezza.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pezza.DataAccess
+{
+    public class Class1
+    {
+    }
+}
